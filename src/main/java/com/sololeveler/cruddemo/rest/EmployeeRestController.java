cbd9cf43486bb5f1318ee2023b309bee7052b5b1
DESKTOP-L4FFAHV\amrnash <amrnashaat8@gmail.com>
2024-11-24 07:28:35 +0200

    Add update and delete employee

--- src/main/java/com/sololeveler/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/sololeveler/cruddemo/rest/EmployeeRestController.java
@@ -3,9 +3,7 @@ package com.sololeveler.cruddemo.rest;
 import com.sololeveler.cruddemo.entity.Employee;
 import com.sololeveler.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,28 @@ public class EmployeeRestController {
     @GetMapping("")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/{employeeID}")
+    public  Employee getEmployee(@PathVariable int employeeID) {
+        Employee employee = employeeService.findById(employeeID);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee ID not found - " + employeeID);
+        }
+
+        return employee;
+    }
+
+    @PostMapping("")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        employee.setId(0);
+        Employee dbEmployee = employeeService.save(employee);
+        return dbEmployee;
+    }
+
+    @PutMapping("")
+    public Employee updatedEmployee(@RequestBody Employee employee) {
+        return employeeService.save(employee);
     }
 }
